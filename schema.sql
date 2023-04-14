be4f6fd2c751f4d07ddad46ce128221589760beb
Patinho <oie.eu.sou.um@gmail.com>
2023-04-14 00:45:07 -0300

    Adicionar novas tabelas

--- schema.sql
+++ schema.sql
@@ -15,3 +15,43 @@ CREATE TABLE IF NOT EXISTS users (
     `role` varchar(255) NOT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS moradias (
+    `id` int AUTO_INCREMENT,
+    `client_id` int NOT NULL REFERENCES users(`id`),
+    `value` int NOT NULL,
+    `address` varchar(255) NOT NULL,
+    `type` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS inquilinos (
+    `id` int REFERENCES users(`id`),
+    `name` varchar(255) NOT NULL,
+    `telephone` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `moradia_id` int NOT NULL REFERENCES moradias(`id`),
+    `taxa_juros` float NOT NULL DEFAULT 0.2,
+    `data_vencimento` date NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS pagamentos (
+    `id` int AUTO_INCREMENT,
+    `inquilino_id` int NOT NULL REFERENCES inquilinos(`id`),
+    `value` int NOT NULL,
+    `data_vencimento` date NOT NULL,
+    `data_pagamento` date,
+    PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS features (
+    `id` int AUTO_INCREMENT,
+    `category` varchar(255) NOT NULL,
+    `min_perm_level` int NOT NULL,
+    `description` varchar(255) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `import_url` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
