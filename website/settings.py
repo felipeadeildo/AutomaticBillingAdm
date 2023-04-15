d48d42f1074c73193314550b3538eca462d4d544
Patinho <oie.eu.sou.um@gmail.com>
2023-04-15 20:30:43 -0300

    Erro de Português

--- website/settings.py
+++ website/settings.py
@@ -2,4 +2,4 @@ EMPRESA_MIN_PERM_LEVEL=80
 """Permissão mínima para acessar funcionalidades de 'empresa'"""
 
 CLIENTE_PERM_LEVEL=50
-"""Permissão mínima nescessária para acessr funcionalidade de 'cliente'"""
+"""Permissão mínima nescessária para acessar funcionalidades de 'cliente'"""
